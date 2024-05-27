31a4c6b9563bc17c02f4b9fd977711d857831a89
ashishsen003 <ashishsen997@gmail.com>
2024-05-27 22:30:28 +0530

    added input component and updated home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
+import Input from '@/components/Input'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className='text-green-300'>Home page</div>
+    <div className='max-w-xl mx-auto border-r border-l min-h-screen'>
+      <div className='py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
+        <h2 className='text-lg sm:text-xl font-bold'>Home</h2>
+      </div>
+      <Input />
+    </div>
   )
 }
 
