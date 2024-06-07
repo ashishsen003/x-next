60ce720aa214687210e4261305c7de0ae0f20962
ashishsen003 <ashishsen997@gmail.com>
2024-06-07 22:10:09 +0530

    added delete post feature for posts

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -18,7 +18,7 @@ import {
 } from "firebase/firestore";
 import { app } from "../firebase";
 
-const Icons = ({ id }) => {
+const Icons = ({ id, uid }) => {
   const { data: session } = useSession();
   const [isLiked, setIsLiked] = useState(false);
   const [likes, setLikes] = useState([]);
@@ -36,6 +36,23 @@ const Icons = ({ id }) => {
       }
     } else {
       signIn();
+    }
+  };
+
+  const deletePost = async () => {
+    if (window.confirm("Are you sure you want to delete this post?")) {
+      if (session?.user?.uid === uid) {
+        deleteDoc(doc(db, "posts", id))
+          .then(() => {
+            console.log("Document successfully deleted!");
+            window.location.reload();
+          })
+          .catch((error) => {
+            console.error("Error removing document: ", error);
+          });
+      } else {
+        alert('You are not authorized to delete this post')
+      }
     }
   };
 
@@ -66,9 +83,18 @@ const Icons = ({ id }) => {
             className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"
           />
         )}
-        {likes.length > 0 && <span className={`text-xs ${isLiked && 'text-red-600'}`}>{likes.length}</span>}
+        {likes.length > 0 && (
+          <span className={`text-xs ${isLiked && "text-red-600"}`}>
+            {likes.length}
+          </span>
+        )}
       </div>
-      <HiOutlineTrash className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100" />
+      {session?.user?.uid === uid && (
+        <HiOutlineTrash
+          onClick={deletePost}
+          className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"
+        />
+      )}
     </div>
   );
 };
