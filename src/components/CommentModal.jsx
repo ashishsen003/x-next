42a4cb9a30026f827120f41395b4129b1b47ac2f
ashishsen003 <ashishsen997@gmail.com>
2024-06-09 21:18:03 +0530

    linked comments to the post Id's

--- src/components/CommentModal.jsx
+++ src/components/CommentModal.jsx
@@ -1,15 +1,16 @@
 'use client'
 import React from 'react'
 import {useRecoilState} from 'recoil'
-import {modalState} from '../atom/modalAtom'
+import {modalState, postIdState} from '../atom/modalAtom'
 
 const CommentModal = () => {
     const [open, setopen] = useRecoilState(modalState)
+    const [postId, setPostId] = useRecoilState(postIdState)
 
   return (
     <div>
         <h1>Comment Modal</h1>
-        {open && <h1>The modal is open</h1>}
+        {open && <h1>{postId}</h1>}
     </div>
   )
 }
