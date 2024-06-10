6c89be64a52f3c79f7a633d094233cd6632e68aa
ashishsen003 <ashishsen997@gmail.com>
2024-06-10 23:42:46 +0530

    added comment modal

--- src/components/CommentModal.jsx
+++ src/components/CommentModal.jsx
@@ -2,15 +2,27 @@
 import React from 'react'
 import {useRecoilState} from 'recoil'
 import {modalState, postIdState} from '../atom/modalAtom'
+import Modal from 'react-modal'
+import {HiX} from 'react-icons/hi'
+const  {useSession} = require('next-auth/react')
 
 const CommentModal = () => {
     const [open, setopen] = useRecoilState(modalState)
     const [postId, setPostId] = useRecoilState(postIdState)
+    const {data: session} = useSession()
 
   return (
     <div>
-        <h1>Comment Modal</h1>
-        {open && <h1>{postId}</h1>}
+        {open && (
+          <Modal 
+          isOpen={open}
+          onRequestClose={()=>setopen(false)}
+          ariaHideApp={false}
+          className={'max-w-lg w-90% absolute top-24 left-[-50%] translate-x-[-50%] bg-white border-2 border-gray-200 rounded-xl shadow-md  '}
+          >
+            
+          </Modal>
+        )}
     </div>
   )
 }
