0ae491816ec62922fafd51184203f0f828954a5d
ashishsen003 <ashishsen997@gmail.com>
2024-06-11 19:57:20 +0530

    added styling and fixed the css of modal

--- src/components/CommentModal.jsx
+++ src/components/CommentModal.jsx
@@ -7,7 +7,7 @@ import {HiX} from 'react-icons/hi'
 const  {useSession} = require('next-auth/react')
 
 const CommentModal = () => {
-    const [open, setopen] = useRecoilState(modalState)
+    const [open, setOpen] = useRecoilState(modalState)
     const [postId, setPostId] = useRecoilState(postIdState)
     const {data: session} = useSession()
 
@@ -16,11 +16,16 @@ const CommentModal = () => {
         {open && (
           <Modal 
           isOpen={open}
-          onRequestClose={()=>setopen(false)}
+          onRequestClose={()=>setOpen(false)}
           ariaHideApp={false}
-          className={'max-w-lg w-90% absolute top-24 left-[-50%] translate-x-[-50%] bg-white border-2 border-gray-200 rounded-xl shadow-md  '}
+          className='max-w-lg w-[90%] absolute top-24 left-[50%] translate-x-[-50%] bg-white border-2 border-gray-200 rounded-xl shadow-md'
           >
-            
+            <div className='p-4'>
+              <div className='border-b border-gray-200 py-2 px-1.5'>
+                <HiX className='text-2xl text-gray-700 p-1 hover:bg-gray-200 rounded-full cursor-pointer' onClick={()=>setOpen(false)} />
+                  Hello ashish
+              </div>
+            </div>
           </Modal>
         )}
     </div>
