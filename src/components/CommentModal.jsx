e07e340007f19ba2295cd31ae60ec6ace8df4922
ashishsen003 <ashishsen997@gmail.com>
2024-06-12 23:20:10 +0530

    added data in comment modal

--- src/components/CommentModal.jsx
+++ src/components/CommentModal.jsx
@@ -1,15 +1,35 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {useRecoilState} from 'recoil'
 import {modalState, postIdState} from '../atom/modalAtom'
 import Modal from 'react-modal'
 import {HiX} from 'react-icons/hi'
 const  {useSession} = require('next-auth/react')
+import {app} from '../firebase'
+import { doc, getFirestore, onSnapshot } from 'firebase/firestore'
 
 const CommentModal = () => {
     const [open, setOpen] = useRecoilState(modalState)
     const [postId, setPostId] = useRecoilState(postIdState)
+    const [post, setPost] = useState({})
     const {data: session} = useSession()
+    const db =  getFirestore(app)
+
+    useEffect(()=>{
+      if(postId !== ''){
+        const postRef = doc(db, 'posts', postId)
+        const unsubscribe = onSnapshot(
+          postRef, 
+          (snapshot)=>{
+            if(snapshot.exists()){
+              setPost(snapshot.data())
+            } else {
+              console.log('no such document');
+            }
+        })
+        return ()=>unsubscribe()
+      }
+    },[postId])
 
   return (
     <div>
@@ -24,6 +44,10 @@ const CommentModal = () => {
               <div className='border-b border-gray-200 py-2 px-1.5'>
                 <HiX className='text-2xl text-gray-700 p-1 hover:bg-gray-200 rounded-full cursor-pointer' onClick={()=>setOpen(false)} />
                   Hello ashish
+              </div>
+              <div className='p-2 flex items-center space-x-1 relative'>
+                <span className='w-0.5 h-full z-[-1] absolsute left-8 top-11 bg-gray-300' />
+                <img src={post?.profileImg} alt="" />
               </div>
             </div>
           </Modal>
